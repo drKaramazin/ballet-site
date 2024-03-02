8c3f633288f82f23b25594049b9b276325427a68
nick <nick.grishkin@gmail.com>
2024-03-02 20:43:53 +0500

    factories in the multiscene example

--- src/docs/examples/multiscene/multiscene.ts
+++ src/docs/examples/multiscene/multiscene.ts
@@ -1,97 +1,79 @@
-import {
-  Ballet,
-  StaticActor,
-  TimeFrame,
-  MoveMotion,
-  StickyPlatformScene,
-} from "ballet";
+import { ballet } from "ballet";
 
-const box1 = new StaticActor(
-  document.getElementById('box1')!,
-  {
-    initSize: false,
-    initOpacity: false,
-  }
-);
-
-const box2 = new StaticActor(
-  document.getElementById('box2')!,
-  {
-    initSize: false,
-    initOpacity: false,
-  }
-);
-
-const box3_1 = new StaticActor(
-  document.getElementById('box3-1')!,
-  {
-    initSize: false,
-    initOpacity: false,
-  }
-);
-
-const box3_2 = new StaticActor(
-  document.getElementById('box3-2')!,
-  {
-    initSize: false,
-    initOpacity: false,
-  }
-);
-
-box1.addFrames([
-  new TimeFrame(new MoveMotion({
-    startX: () => 0,
-    endX: (w, h) => w - 30,
-    startY: () => 0,
-    endY: (w, h) => h,
-  }), (w, h) => 0, (w, h) => h),
-]);
-
-box2.addFrames([
-  new TimeFrame(new MoveMotion({
-    startX: (w, h) => w - 30,
-    endX: () => 0,
-    startY: () => 0,
-    endY: (w, h) => h,
-  }), (w, h) => 0, (w, h) => h),
-]);
-
-box3_1.addFrames([
-  new TimeFrame(new MoveMotion({
-    startX: () => 0,
-    endX: (w, h) => w - 30,
-    startY: () => 0,
-    endY: (w, h) => h,
-  }), (w, h) => 0, (w, h) => h),
-]);
-
-box3_2.addFrames([
-  new TimeFrame(new MoveMotion({
-    startX: (w, h) => w - 30,
-    endX: () => 0,
-    startY: () => 0,
-    endY: (w, h) => h,
-  }), (w, h) => 0, (w, h) => h),
-]);
-
-const scene1 = new StickyPlatformScene(
-  document.getElementById('scene1')!,
-  (w, h) => h,
-);
-
-const scene2 = new StickyPlatformScene(
-  document.getElementById('scene2')!,
-  (w, h) => h,
-);
-
-const scene3 = new StickyPlatformScene(
-  document.getElementById('scene3')!,
-  (w, h) => h,
-);
-
-scene1.add(box1);
-scene2.add(box2);
-scene3.add(box3_1);
-scene3.add(box3_2);
-
-const bt = new Ballet([scene1, scene2, scene3]);
+ballet([{
+  element: '#scene1',
+  height: (w, h) => h,
+  actors: {
+    element: '#box1',
+    options: {
+      initSize: false,
+      initOpacity: false,
+    },
+    frames: {
+      move: {
+        startX: () => 0,
+        endX: (w) => w - 30,
+        startY: () => 0,
+        endY: (w, h) => h,
+      },
+      start: () => 0,
+      end: (w, h) => h,
+    },
+  },
+}, {
+  element: '#scene2',
+  height: (w, h) => h,
+  actors: {
+    element: '#box2',
+    options: {
+      initSize: false,
+      initOpacity: false,
+    },
+    frames: {
+      move: {
+        startX: (w) => w - 30,
+        endX: () => 0,
+        startY: () => 0,
+        endY: (w, h) => h,
+      },
+      start: () => 0,
+      end: (w, h) => h,
+    },
+  },
+}, {
+  element: '#scene3',
+  height: (w, h) => h,
+  actors: [{
+    element: '#box3-1',
+    options: {
+      initSize: false,
+      initOpacity: false,
+    },
+    frames: {
+      move: {
+        startX: () => 0,
+        endX: (w) => w - 30,
+        startY: () => 0,
+        endY: (w, h) => h,
+      },
+      start: () => 0,
+      end: (w, h) => h,
+    },
+  }, {
+    element: '#box3-2',
+    options: {
+      initSize: false,
+      initOpacity: false,
+    },
+    frames: {
+      move: {
+        startX: (w) => w - 30,
+        endX: () => 0,
+        startY: () => 0,
+        endY: (w, h) => h,
+      },
+      start: () => 0,
+      end: (w, h) => h,
+    },
+  }],
+}]);
